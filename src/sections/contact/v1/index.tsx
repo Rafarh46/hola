607cc1e52996b3ce67d5417d4aa4611b7af5d3f9
Cecilia Hurtado González <romerorh46@gmail.com>
2024-06-29 16:13:03 -0700

    Test Husky3

--- src/sections/contact/v1/index.tsx
+++ src/sections/contact/v1/index.tsx
@@ -1,6 +1,6 @@
 import { TeamSectionProps } from '@/src/sections/team/v1';
 
-export const teamSectionData: TeamSectionProps = {
+export const ContactSection: TeamSectionProps = {
   sectionHeading: {
     subtitle: 'Presnecia Nacional',
     title: 'Presnecia Nacional',
