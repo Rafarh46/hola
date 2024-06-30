acabb8c84fdc884eb7054fb5bcf84993ca650d53
Cecilia Hurtado González <romerorh46@gmail.com>
2024-06-29 20:09:01 -0700

    Test Husky2

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import { AOSInit } from '@/src/utils/aos';
 import { ScrollToTopButton } from '@/src/components/scroll-to-top';
 import { cn } from '@/src/utils/shadcn';
 import { Toaster } from 'sonner';
-import { ThemeProvider } from '@/src/components/theme-provider';
 import { ModeToggle } from '@/src/components/mode-toggle';
 
 interface Props {
