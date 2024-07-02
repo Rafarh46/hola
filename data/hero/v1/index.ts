3757ae760daee5c09bb7e44fa96a175bf1f43827
Cecilia Hurtado González <romerorh46@gmail.com>
2024-07-02 13:01:10 -0700

    Test Husky3

--- data/hero/v1/index.ts
+++ data/hero/v1/index.ts
@@ -8,32 +8,7 @@ export const heroData: HeroProps = {
         alt: 'hero 1',
       },
       title: 'Sistemas Detección de Incendio',
-      button: {
-        label: 'Contacto',
-        href: '/contact',
-      },
     },
-    {
-      image: {
-        src: '/assets/images/hero/hero-11.png',
-        alt: 'hero 1',
-      },
-      title: 'Sistemas Detección de Incendio',
-      button: {
-        label: 'Contacto',
-        href: '/contact',
-      },
-    },
-    {
-      image: {
-        src: '/assets/images/hero/hero-12.png',
-        alt: 'hero 1',
-      },
-      title: 'Sistemas Detección de Incendio',
-      button: {
-        label: 'Contacto',
-        href: '/contact',
-      },
-    },
+
   ],
 };
